fc30f503d93a89c535427a813ab9b1ec45e096b7
kofi1123 <kofiquansah1123@gmail.com>
2024-05-10 16:28:51 -0700

    TFS 1.4.2 and otclient setup + Question 5

---
+++ forgottenserver-1.4.2/data/spells/scripts/attack/winter_storm.lua
@@ -0,0 +1,29 @@
+local combat = Combat()
+local area = createCombatArea({
+	{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+	{0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+	{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+	{0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+	{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}
+})
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat:setArea(area)
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 5.5) + 25
+	local max = (level / 5) + (magicLevel * 11) + 50
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
